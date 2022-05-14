417ad262de684a5f81dc168fe67b626b75aefabc
kate-kazantseva <ea.kazantseva96@gmail.com>
2022-05-14 08:59:53 -0700

    bug fix for unsubscribed users

--- Listener/app.js
+++ Listener/app.js
@@ -27,31 +27,35 @@ async function notify(job_id) {
     for (i in results.members) {
       let subscriber = await client.json.get(results.members[i]);
       let keys = subscriber.sub_keys["mountain_view_ca"];
-      for (key of keys) {
-        key.toLowerCase();
-        let email_body = "<h2>This might interest you: " + job.job_title + " in " + job.location + "</h2>";
-        email_body = email_body + "<p>Job Type: " + job.job_type + "</p><p>Salary range: " + 
-                     job.salary_range_start +  " - " + job.salary_range_end + " per " + job.salary_type +
-                     "</p><p>Experience: " + job.experience + "</p><p>" + job.description + "<p>Minimum qualifications: " + job.m_qualifications +
-                     "</p><p>Preferred qualifications: " + job.p_qualifications + "</p>" + "<a href='http://localhost:3000/apply/" + job_id +"'><h3>Apply</h3></a>";
-        if (title.includes(key)) {
-          const mailOptions = {
-            from: 'jobsearch@gmail.com',
-            to: results.members[i],
-            subject: 'New Job Notification',
-            html: email_body
-          };
-        
-          transporter.sendMail(mailOptions, function(error, info) {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
+      if (keys != null || keys != undefined) {
+        for (key of keys) {
+          key.toLowerCase();
+          let email_body = "<h2>This might interest you: " + job.job_title + " in " + job.location + "</h2>";
+          email_body = email_body + "<p>Job Type: " + job.job_type + "</p><p>Salary range: " + 
+                      job.salary_range_start +  " - " + job.salary_range_end + " per " + job.salary_type +
+                      "</p><p>Experience: " + job.experience + "</p><p>" + job.description + "<p>Minimum qualifications: " + job.m_qualifications +
+                      "</p><p>Preferred qualifications: " + job.p_qualifications + "</p>" + "<a href='http://localhost:3000/apply/" + job_id +"'><h3>Apply</h3></a>";
+          if (title.includes(key)) {
+            const mailOptions = {
+              from: 'jobsearch@gmail.com',
+              to: results.members[i],
+              subject: 'New Job Notification',
+              html: email_body
+            };
+          
+            transporter.sendMail(mailOptions, function(error, info) {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log('Email sent: ' + info.response);
+              }
+            });
 
-          break;
+            break;
+          }
         }
+      } else {
+        client.sRem("mountain_view_ca", results.members[i]);
       }
     }
 }
